997f576766a53d82a49268d042a2dd32ca386020
ScaratP <wsp617617@gmail.com>
2025-09-15 15:39:36 +0800

    sp/250915/下修api版本至21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.wifiindoorsystem"
-        minSdk = 31
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
